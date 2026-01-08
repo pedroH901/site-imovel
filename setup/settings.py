1a1348eb08bffcf20541a195e8db604088cba2df
pedro henrique dias ferreira <pedro@gemail.com>
2026-01-08 17:33:28 -0300

    pequenos polimentos, preço dos produtos humanizado e sistema de limpar os filtros de busca

--- setup/settings.py
+++ setup/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
     'core',
     'accounts',
     'imoveis',
@@ -105,13 +106,15 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
 USE_TZ = True
+
+USE_THOUSAND_SEPARATOR = True
 
 
 # Static files (CSS, JavaScript, Images)
